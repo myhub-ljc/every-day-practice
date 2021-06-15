56d04ab974040203fbf08e375820ae42464f3d88
myhub-ljc <2321170474@qq.com>
2021-06-15 13:39:01 +0800

    vue案例练习—商品列表的切换

---
+++ js训练/vue案例练习—商品列表切换/App.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <product-list :titles="titles" @btnClick="btnClick"></product-list>
+        <h2>{{titles[currentIndex]}}</h2>
+    </div>
+</template>
+
+<script>
+    import ProductList from "./productList.vue";
+    export default {
+        components: {
+            ProductList
+        },
+        data() {
+            return {
+                titles: ["新品", "潮流", "精选"],
+                currentIndex: 0
+            }
+        },
+        methods: {
+            btnClick(index) {
+                this.currentIndex = index;
+            }
+        }
+    }
+</script>
---
+++ js训练/vue案例练习—商品列表切换/productList.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="list">
+        <div class="item" 
+             :class="{active: currentIndex === index}"
+             v-for="(title, index) in titles" 
+             :key="title">
+            <span @click="btnClick(index)">{{title}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default{
+        emits: ["btnClick"],
+        props: {
+            titles: {
+                type: Array,
+                default() {
+                    return []
+                }
+            }
+        },
+        data() {
+            return {
+              currentIndex: 0
+            }
+        },
+        methods: {
+            btnClick(index) {
+                // console.log(index);
+                this.currentIndex = index;
+                this.$emit("btnClick", index)
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .list{
+        display: flex;
+        width: 200px;
+    }
+
+    .item{
+        flex: 1;
+    }
+
+    .item.active{
+        color: red;
+    }
+    
+    .item.active span{
+        border-bottom: 1px solid red;
+    }
+</style>
