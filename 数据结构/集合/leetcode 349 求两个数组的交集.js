b060723d87e1bafd70a4e1243b78fe4f0ef3c20b
myhub-ljc <2321170474@qq.com>
2021-07-01 20:43:20 +0800

    集合数据结构的复习

--- 数据结构/集合/leetcode 349 求两个数组的交集.js
+++ 数据结构/集合/leetcode 349 求两个数组的交集.js
@@ -1,19 +1,13 @@
 /**
- * 解题步骤：
- *      使用集合对nums1去重
- *      遍历nums1，筛选出nums2也包含的值
- */
-/**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
  var intersection = function(nums1, nums2) {
-    //先利用集合对两个数组进行去重
-    const set1 = [...new Set(nums1)];
-    const set2 = new Set(nums2);
- 
-    //遍历求交集
- //    return set1.filter(item => set2.has(item));
-     return set1.filter(item => set2.has(item));
- };
+    const arr = [...new Set(nums1)]; //先利用集合去重再转回数组
+    const arr2 = new Set(nums2); //利用集合进行去重 
+
+    //判断arr2中是否含有arr1中的元素即二者的交集
+    const arr3 = arr.filter(item => arr2.has(item)) 
+    return arr3;
+};
