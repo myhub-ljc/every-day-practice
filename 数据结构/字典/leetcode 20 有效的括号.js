606586ce7f7547e1ad4ffb4c569cb4dac0caf0ea
myhub-ljc <2321170474@qq.com>
2021-07-01 21:24:47 +0800

    字典实现求交集和有效的括号问题

--- 数据结构/字典/leetcode 20 有效的括号.js
+++ 数据结构/字典/leetcode 20 有效的括号.js
@@ -1,50 +1,26 @@
-//上次使用的是栈，下面使用字典实现
 /**
  * @param {string} s
  * @return {boolean}
  */
  var isValid = function(s) {
-    //方法一：使用栈实现
-// if (s.length % 2 === 1) {
-//     return false;
-// }
-
-// const stack = [];
-
-// for (let i = 0; i < s.length; i++) {
-//     const a = s[i];
-//     if (a === '(' || a === '[' || a === '{') {
-//         stack.push(a);
-//     } else {
-//         const b = stack[stack.length - 1];
-//         if (
-//             (b === '(' && a === ')') ||
-//             (b === '[' && a === ']') ||
-//             (b === '{' && a === '}')
-//         ) {
-//             stack.pop();
-//         } else {
-//             return false;
-//         }
-//     }
-// }
-// return stack.length === 0 ? true : false;
-
-    //方法二：使用字典实现
-    //创建一个字典
+    //新建一个字典map用来存放三种类型的括号
     const map = new Map();
-    //将所有括号存放进字典中
-    map.set('(', ')');
-    map.set('[', ']');
-    map.set('{', '}');
+    map.set("(", ")");
+    map.set("{", "}");
+    map.set("[", "]");
 
+    //新建一个空栈
     const stack = [];
+    //循环遍历拿到s中的每一个元素
     for(let i = 0; i < s.length; i++) {
         const a = s[i];
+        //判断字典中是否有该值，有则push进入stack中
         if(map.get(a)) {
             stack.push(a);
         } else {
+            //拿到栈顶元素
             const b = stack[stack.length - 1];
+            //如果栈顶元素与s中的某个元素相同，则pop出去
             if(map.get(b) === a) {
                 stack.pop();
             } else {
@@ -52,5 +28,5 @@
             }
         }
     }
-    return stack.length === 0 ? true : false;
+    return stack.length === 0;
 };
--- 数据结构/字典/leetcode 349 求两个数组的交集.js
+++ 数据结构/字典/leetcode 349 求两个数组的交集.js
@@ -1,41 +1,27 @@
-//上次使用的是集合，下面我们使用字典
-/**
- * 解题步骤：
- *      新建一个字典，遍历nums1，填充字典
- *      遍历nums2，遇到字典里的值就选出来，并从字典中删除
- */
-
 /**
  * @param {number[]} nums1
  * @param {number[]} nums2
  * @return {number[]}
  */
  var intersection = function(nums1, nums2) {
-    // //方法一：使用集合
-    //    //先利用集合对两个数组进行去重
-    //    const set1 = [...new Set(nums1)];
-    //    const set2 = new Set(nums2);
-    
-    //    //遍历求交集
-    // //    return set1.filter(item => set2.has(item));
-    //     return set1.filter(item => set2.has(item));
-    
-    //方法二：使用字典
-     //创建一个字典
+    //新建一个字典
     const map = new Map();
-    //遍历并填充
+    //遍历nums1数组
     nums1.forEach(n => {
+        //填充字典
         map.set(n, true);
     });
 
-    const arr = [];
-    //遍历判断nums2中和nums1中相同的值
+    //新建一个数组用于存放交集
+    var arr = [];
+    //对nums2数组进行循环遍历
     nums2.forEach(n => {
+        //如果字典中已经存在该元素则push进数组并且从字典中删除
         if(map.get(n)) {
             arr.push(n);
             map.delete(n);
-        };
+        }
     })
-    return arr;
 
+    return arr;
 };
