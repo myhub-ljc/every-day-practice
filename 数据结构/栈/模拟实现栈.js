4bd8a3a20d24f7db610611ef3021bfaa5cb01b55
myhub-ljc <2321170474@qq.com>
2021-11-01 12:19:25 +0800

    重置

--- 数据结构/栈/模拟实现栈.js
+++
@@ -1,7 +0,0 @@
-//我们可以借助数组Array来模拟实现栈
-//先创建一个空栈
-const stack = [];
-
-stack.push(2);
-let result = stack.pop();
-console.log(result)
--- 数据结构/队列/模拟实现队列.js
+++
@@ -1,5 +0,0 @@
-//js中是没有队列这种数据结构的，但是我们可以使用Array来实现队列的所以功能
-const queue = [];
-
-queue.push(1);
-queue.shift();
